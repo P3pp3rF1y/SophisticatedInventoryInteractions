bde17e609a127ad3e7d0e301a5211dfab45aa48a
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-19 21:10:10 +0200

    feat: ✨ Added in-game NeoForge config GUI support and labels

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedinventoryinteractions/client/gui/InventoryInteractionsTranslationHelper.java
@@ -0,0 +1,12 @@
+package net.p3pp3rf1y.sophisticatedinventoryinteractions.client.gui;
+
+import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.TranslationHelper;
+import net.p3pp3rf1y.sophisticatedinventoryinteractions.SophisticatedInventoryInteractions;
+
+public class InventoryInteractionsTranslationHelper extends TranslationHelper {
+	public static final InventoryInteractionsTranslationHelper INSTANCE = new InventoryInteractionsTranslationHelper();
+
+	private InventoryInteractionsTranslationHelper() {
+		super(SophisticatedInventoryInteractions.MOD_ID);
+	}
+}
