bdc04d31fbdb097f82eee33e3b75cd1c49c92c1d
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-05-24 09:41:42 +0200

    feat: ✨ Added memory for sort by settings

--- src/main/java/net/p3pp3rf1y/sophisticatedinventoryinteractions/init/ModPayloads.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedinventoryinteractions/init/ModPayloads.java
@@ -4,6 +4,9 @@ import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.p3pp3rf1y.sophisticatedinventoryinteractions.SophisticatedInventoryInteractions;
 import net.p3pp3rf1y.sophisticatedinventoryinteractions.network.ContainerInteractionPayload;
+import net.p3pp3rf1y.sophisticatedinventoryinteractions.network.RequestSortMemoryPayload;
+import net.p3pp3rf1y.sophisticatedinventoryinteractions.network.SetSortMemoryPayload;
+import net.p3pp3rf1y.sophisticatedinventoryinteractions.network.SyncSortMemoryPayload;
 
 public class ModPayloads {
 	private ModPayloads() {
@@ -12,5 +15,8 @@ public class ModPayloads {
 	public static void registerPayloads(final RegisterPayloadHandlersEvent event) {
 		PayloadRegistrar registrar = event.registrar(SophisticatedInventoryInteractions.MOD_ID).versioned(SophisticatedInventoryInteractions.getNetworkProtocolVersion());
 		registrar.playToServer(ContainerInteractionPayload.TYPE, ContainerInteractionPayload.STREAM_CODEC, ContainerInteractionPayload::handlePayload);
+		registrar.playToServer(RequestSortMemoryPayload.TYPE, RequestSortMemoryPayload.STREAM_CODEC, RequestSortMemoryPayload::handlePayload);
+		registrar.playToServer(SetSortMemoryPayload.TYPE, SetSortMemoryPayload.STREAM_CODEC, SetSortMemoryPayload::handlePayload);
+		registrar.playToClient(SyncSortMemoryPayload.TYPE, SyncSortMemoryPayload.STREAM_CODEC, SyncSortMemoryPayload::handlePayload);
 	}
 }
