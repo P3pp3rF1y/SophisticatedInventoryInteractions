93fd73c5e2817fbd84a698cf387dbd2b2f56f0fe
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-19 11:17:26 +0200

    fix: 🐛 Fixed sort keybinds in vanilla inventories

--- src/main/java/net/p3pp3rf1y/sophisticatedinventoryinteractions/client/ScreenInteractionInjector.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedinventoryinteractions/client/ScreenInteractionInjector.java
@@ -14,6 +14,7 @@ import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.inventory.Slot;
 import net.neoforged.neoforge.client.event.ScreenEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
+import net.p3pp3rf1y.sophisticatedcore.client.ClientEventHandler;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.controls.*;
 import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.*;
@@ -28,6 +29,7 @@ import net.p3pp3rf1y.sophisticatedinventoryinteractions.common.slots.SlotRegions
 import net.p3pp3rf1y.sophisticatedinventoryinteractions.network.ContainerInteractionPayload;
 import net.p3pp3rf1y.sophisticatedinventoryinteractions.network.RequestSortMemoryPayload;
 import net.p3pp3rf1y.sophisticatedinventoryinteractions.network.SetSortMemoryPayload;
+import org.lwjgl.glfw.GLFW;
 import org.lwjgl.system.MemoryUtil;
 
 import javax.annotation.Nullable;
@@ -332,7 +334,7 @@ public class ScreenInteractionInjector {
 			return;
 		}
 		if (tryHandleSortKeybind(event.getScreen(), InputConstants.Type.MOUSE.getOrCreate(event.getButton()))
-				|| tryHandleTransferKeybind(event.getScreen(), InputConstants.Type.MOUSE.getOrCreate(event.getButton()))) {
+				|| tryHandleTransferKeybind(event.getScreen(), InputConstants.Type.MOUSE.getOrCreate(event.getButton()), Screen.hasShiftDown())) {
 			event.setCanceled(true);
 			return;
 		}
@@ -366,12 +368,13 @@ public class ScreenInteractionInjector {
 
 	public void onKeyPressed(ScreenEvent.KeyPressed.Pre event) {
 		InputConstants.Key key = InputConstants.getKey(event.getKeyCode(), event.getScanCode());
-		if (tryHandleSortKeybind(event.getScreen(), key) || tryHandleTransferKeybind(event.getScreen(), key)) {
+		boolean shiftDown = Screen.hasShiftDown() || (event.getModifiers() & GLFW.GLFW_MOD_SHIFT) != 0;
+		if (tryHandleSortKeybind(event.getScreen(), key) || tryHandleTransferKeybind(event.getScreen(), key, shiftDown)) {
 			event.setCanceled(true);
 		}
 	}
 
-	private boolean tryHandleTransferKeybind(Screen screen, InputConstants.Key inputKey) {
+	private boolean tryHandleTransferKeybind(Screen screen, InputConstants.Key inputKey, boolean shiftDown) {
 		if (!(screen instanceof AbstractContainerScreen<?> containerScreen)) {
 			return false;
 		}
@@ -391,7 +394,7 @@ public class ScreenInteractionInjector {
 			return false;
 		}
 
-		PacketDistributor.sendToServer(new ContainerInteractionPayload(actionType, !Screen.hasShiftDown(), SortBy.NAME));
+		PacketDistributor.sendToServer(new ContainerInteractionPayload(actionType, !shiftDown, SortBy.NAME));
 		GuiSoundHelper.playButtonClickSound();
 		return true;
 	}
@@ -441,7 +444,7 @@ public class ScreenInteractionInjector {
 	}
 
 	private boolean matchesSortKeybind(InputConstants.Key inputKey) {
-		return matchesKeybind(net.p3pp3rf1y.sophisticatedcore.client.ClientEventHandler.SORT_KEYBIND, inputKey);
+		return !ClientEventHandler.SORT_KEYBIND.isUnbound() && ClientEventHandler.SORT_KEYBIND.getKey().equals(inputKey);
 	}
 
 	private boolean matchesTransferToStorageKeybind(InputConstants.Key inputKey) {
