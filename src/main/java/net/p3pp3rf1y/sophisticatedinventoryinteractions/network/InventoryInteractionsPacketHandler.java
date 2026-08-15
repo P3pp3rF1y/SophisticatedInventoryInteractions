01749ed2b566e2bd2a0af0ed2a9b92f2dd559a02
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-15 19:54:32 +0200

    fix: 🐛 Fixed unsafe network packet handling

--- src/main/java/net/p3pp3rf1y/sophisticatedinventoryinteractions/network/InventoryInteractionsPacketHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedinventoryinteractions/network/InventoryInteractionsPacketHandler.java
@@ -1,5 +1,6 @@
 package net.p3pp3rf1y.sophisticatedinventoryinteractions.network;
 
+import net.minecraftforge.network.NetworkDirection;
 import net.p3pp3rf1y.sophisticatedcore.network.PacketHandler;
 import net.p3pp3rf1y.sophisticatedinventoryinteractions.SophisticatedInventoryInteractions;
 
@@ -13,10 +14,12 @@ public class InventoryInteractionsPacketHandler extends PacketHandler {
 	@Override
 	public void registerMessages() {
 		registerMessage(ContainerInteractionPayload.class, ContainerInteractionPayload::encode, ContainerInteractionPayload::decode,
-				ContainerInteractionPayload::onMessage);
-		registerMessage(RequestSortMemoryPayload.class, RequestSortMemoryPayload::encode, RequestSortMemoryPayload::decode,
-				RequestSortMemoryPayload::onMessage);
-		registerMessage(SetSortMemoryPayload.class, SetSortMemoryPayload::encode, SetSortMemoryPayload::decode, SetSortMemoryPayload::onMessage);
-		registerMessage(SyncSortMemoryPayload.class, SyncSortMemoryPayload::encode, SyncSortMemoryPayload::decode, SyncSortMemoryPayload::onMessage);
+				ContainerInteractionPayload::onMessage, NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(RequestSortMemoryPayload.class, RequestSortMemoryPayload::encode, RequestSortMemoryPayload::decode, RequestSortMemoryPayload::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(SetSortMemoryPayload.class, SetSortMemoryPayload::encode, SetSortMemoryPayload::decode, SetSortMemoryPayload::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(SyncSortMemoryPayload.class, SyncSortMemoryPayload::encode, SyncSortMemoryPayload::decode, SyncSortMemoryPayload::onMessage,
+				NetworkDirection.PLAY_TO_CLIENT);
 	}
 }
